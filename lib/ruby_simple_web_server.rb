a1b4dd66d9dcff0d103814ea76ba02fe87ba4e15
fujiw <fujiw10@gmail.com>
2021-09-24 02:24:36 +0900

    init

---
+++ lib/ruby_simple_web_server.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ruby_simple_web_server/version"
+
+module RubySimpleWebServer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ruby_simple_web_server/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RubySimpleWebServer
+  VERSION = "0.1.0"
+end
